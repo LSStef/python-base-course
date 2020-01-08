2d1dc36a4612eb5a563c92c37188af9f3096bc9a
Степан <smeta60@gmail.com>
2020-01-09 00:16:40 +0300

    ок

--- урок 1.5.py
+++ урок 1.5.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = 'http://www.cbr-xml-daily.ru/daily_jsonp.js'
+url = 'http://www.cbr-xml-daily.ru/daily_json.js'
 response = requests.get(url)
 data = json.loads(response.text)
 print(data)
